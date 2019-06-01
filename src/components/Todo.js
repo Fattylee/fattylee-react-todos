d560584967aca6347fcfb0b9a507dc580eec66f4
Fatai Balogun <fattylee.remod@gmail.com>
2019-06-01 21:57:14 +0100

    basic create react setup

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+class Todo extends Component {
+  render () {
+    return (
+      <Fragment>
+        <h1>World best Todo App<h1>
+      </Fragment>
+    )
+  }
+}
+
+export default Todo;
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const template = (
+<h1>Hello React</h1>
+);
+
+ReactDOM.render(template, document.getElementById('root'));
