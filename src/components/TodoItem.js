7b31b2c3bffb897eec840435c557523fb9051190
Fatai Balogun <fattylee.remod@gmail.com>
2019-06-07 11:11:35 +0100

    fix todo list

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,14 +4,18 @@ import React from 'react';
 const TodoItem = ({ todo: {content, id}, deleteTodo, counter }) => {
   return (
      <li className="collection-item">
-       <div><span className='bold'>{counter}</span>. {content}
+       <div>
+         <span className='bold'>{counter}</span>. {content}
        <a 
        onClick={() => {
          deleteTodo(id);
        }}
-       className="secondary-content pointer"><i className="material-icons">delete</i></a></div></li>
+       className="secondary-content">
+       <i className="material-icons">delete</i>
+       </a>
+       </div>
+     </li>
     )
 }
 
 export default TodoItem;
-
